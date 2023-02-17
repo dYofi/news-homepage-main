1b55de431ce32142600afbcad13b67c39caae3d8
Yogi Firdaus <yogifirdaus24@gmail.com>
2023-02-17 14:33:23 +0700

    masi ada bug

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    // fontSize: {
+    //   base: '15px',
+    // },
+    extend: {
+      colors: {
+        softOrange: 'hsl(35, 77%, 62%)',
+        softRed: 'hsl(5, 85%, 63%)',
+        offWhite: 'hsl(36, 100%, 99%)',
+        grayishBlue: 'hsl(233, 8%, 79%)',
+        darkGrayishBlue: 'hsl(236, 13%, 42%)',
+        veryDarkBlue: 'hsl(240, 100%, 5%)',
+      },
+    },
+  },
+  plugins: [],
+};
